c36e17bd4dff4da88f64dc01581e8a8c4d460cae
artinan <artinan30@protonmail.com>
2024-03-23 11:21:44 -0700

    added camera that follows an entity

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,34 @@ int SCREEN_H = 800;
 
 struct Entity {
     Vector2 position;
+    float radius;
+
+    void init(float x, float y, float rad) {
+        position = {x, y};
+        radius = rad;
+    }
 
     void draw() {
-        DrawCircleV(position, 10, RED);
+        DrawCircleV(position, radius, RED);
+    }
+};
+
+struct Viewport {
+    Camera2D camera;
+    Entity* follow;
+
+    void init(Entity* following) {
+        camera = {0};
+        
+        camera.offset = { SCREEN_W / 2.f, SCREEN_H / 2.f };
+        camera.rotation = 0.f;
+        camera.zoom = 1.f;
+
+        follow = following;
+    }
+
+    void update() {
+        camera.target = follow->position;
     }
 };
 
@@ -19,16 +44,25 @@ int main() {
     InitWindow(SCREEN_W, SCREEN_H, "template");
 
     Entity player;
-    player.position = {100, 100};
+    player.init(200, 100, 100);
+
+    Viewport cam;
+    cam.init(&player);
 
     while (!WindowShouldClose()) {
         //float dt = GetFrameTime();
+
+        cam.update();
 
         BeginDrawing();
 
         ClearBackground(BLACK);
 
+        BeginMode2D(cam.camera);
+
         player.draw();
+
+        EndMode2D();
         
         DrawFPS(20, 20);
 
